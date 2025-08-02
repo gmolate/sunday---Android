39a43159fa25fd6f741049a0f7a48c7a89141f9b
gmolate <gmolate@gmail.com>
2025-08-02 16:13:04 -0400

    add /ANDROID android version
    # Sunday - Android Version
    
    This is the Android version of [Sunday](https://github.com/jackjackbits/sunday), a powerful application for tracking UV exposure and Vitamin D generation, rebuilt from the ground up in Kotlin for the Android platform.
    
    This version is designed to be completely self-contained within the `android/` directory.

--- app/src/main/java/com/example/sunday/SundayWidgetReceiver.kt
+++
@@ -1,9 +0,0 @@
-package com.example.sunday
-
-import androidx.glance.appwidget.GlanceAppWidget
-import androidx.glance.appwidget.GlanceAppWidgetReceiver
-import com.example.sunday.ui.SundayWidget
-
-class SundayWidgetReceiver : GlanceAppWidgetReceiver() {
-    override val glanceAppWidget: GlanceAppWidget = SundayWidget()
-}
